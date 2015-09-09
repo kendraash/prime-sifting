4f19950bc34e61cc06d65da254cca05455c53829
Janice Laset-Parkerson & Kendra Ash <student@epicodus.com>
2015-09-09 13:59:11 -0700

    add passing spec for prime numbers 1 and 7

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var primeSifting = function(number) {
+  if(number == 7 || 1)
+    return true;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("primeSifting", function() {
+  it("returns true for the prime numbers 7 or 1", function() {
+    expect(primeSifting(1)).to.equal(true);
+  });
+});
